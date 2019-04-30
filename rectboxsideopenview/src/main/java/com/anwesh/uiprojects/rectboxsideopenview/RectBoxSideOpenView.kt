e92bb2d631492ba42b6b5b70079342d44bd4390e
Anwesh43 <anweshthecool0@gmail.com>
2019-05-01 04:01:39 +0530

    adding RBSONode class

--- rectboxsideopenview/src/main/java/com/anwesh/uiprojects/rectboxsideopenview/RectBoxSideOpenView.kt
+++ rectboxsideopenview/src/main/java/com/anwesh/uiprojects/rectboxsideopenview/RectBoxSideOpenView.kt
@@ -126,4 +126,48 @@ class RectBoxSideOpenView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class RBSONode(var i : Int, val state : State = State()) {
+
+        private var next : RBSONode? = null
+        private var prev : RBSONode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < nodes - 1) {
+                next = RBSONode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawRBSONode(i, state.scale, paint)
+            next?.draw(canvas, paint)
+        }
+
+        fun update(cb : (Int, Float) -> Unit) {
+            state.update {
+                cb(i, it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : RBSONode {
+            var curr : RBSONode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this 
+        }
+    }
 }
