ea8a8fa7535ae9b727b04735e961693325c8aebc
Anwesh43 <anweshthecool0@gmail.com>
2019-05-01 04:04:04 +0530

    adding RectBoxSideOpen class

--- rectboxsideopenview/src/main/java/com/anwesh/uiprojects/rectboxsideopenview/RectBoxSideOpenView.kt
+++ rectboxsideopenview/src/main/java/com/anwesh/uiprojects/rectboxsideopenview/RectBoxSideOpenView.kt
@@ -167,7 +167,31 @@ class RectBoxSideOpenView(ctx : Context) : View(ctx) {
                 return curr
             }
             cb()
-            return this 
+            return this
+        }
+    }
+
+    data class RectBoxSideOpen(var i : Int, val state : State = State()) {
+
+        private val root : RBSONode = RBSONode(0)
+        private var curr : RBSONode = root
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            root.draw(canvas, paint)
+        }
+
+        fun update(cb : (Int, Float) -> Unit) {
+            curr.update {i, scl ->
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(i, scl)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
         }
     }
 }
