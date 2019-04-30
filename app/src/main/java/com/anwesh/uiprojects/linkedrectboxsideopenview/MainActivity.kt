3e316ecbfd97e7ac20e54f04cb87944710395246
Anwesh43 <anweshthecool0@gmail.com>
2019-05-01 04:09:58 +0530

    adding view in activity

--- app/src/main/java/com/anwesh/uiprojects/linkedrectboxsideopenview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkedrectboxsideopenview/MainActivity.kt
@@ -2,11 +2,12 @@ package com.anwesh.uiprojects.linkedrectboxsideopenview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.anwesh.uiprojects.rectboxsideopenview.RectBoxSideOpenView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        RectBoxSideOpenView.create(this)
     }
 }
