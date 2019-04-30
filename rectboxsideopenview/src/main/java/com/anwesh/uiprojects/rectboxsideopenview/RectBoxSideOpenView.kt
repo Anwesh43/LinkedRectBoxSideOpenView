90147e8858421fb1b171a2abed3df56e343394d2
Anwesh43 <anweshthecool0@gmail.com>
2019-05-01 03:42:21 +0530

    adding drawRBSONode canvas extension method

--- rectboxsideopenview/src/main/java/com/anwesh/uiprojects/rectboxsideopenview/RectBoxSideOpenView.kt
+++ rectboxsideopenview/src/main/java/com/anwesh/uiprojects/rectboxsideopenview/RectBoxSideOpenView.kt
@@ -20,6 +20,8 @@ val strokeFactor : Int = 90
 val sizeFactor : Float = 2.9f
 val foreColor : Int = Color.parseColor("#673AB7")
 val backColor : Int = Color.parseColor("#BDBDBD")
+val hFactor : Float = 3f
+val angleDeg : Float = 90f
 
 fun Int.inverse() : Float = 1f / this
 fun Float.scaleFactor() : Float = Math.floor(this / scDiv).toFloat()
@@ -29,4 +31,33 @@ fun Float.mirrorValue(a : Int, b : Int) : Float {
     val k : Float = scaleFactor()
     return (1 - k) * a.inverse() + k * b.inverse()
 }
-fun Float.updateValue(dir : Float, a : Int, b : Int) : Float = mirrorValue(a, b) * dir * scGap 
+fun Float.updateValue(dir : Float, a : Int, b : Int) : Float = mirrorValue(a, b) * dir * scGap
+
+fun Canvas.drawSideOpenLine(size : Float, sc : Float, paint : Paint) {
+    val h : Float = size / hFactor
+    save()
+    translate(-size, -2 * h * sc)
+    drawLine(0f, -h, 0f, h, paint)
+    restore()
+}
+
+fun Canvas.drawRBSONode(i : Int, scale : Float, paint : Paint) {
+    val sc1 : Float = scale.divideScale(0, 2)
+    val sc2 : Float = scale.divideScale(1, 2)
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    val gap : Float = h / (nodes + 1)
+    val size : Float = gap / sizeFactor
+    paint.color = foreColor
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    paint.strokeCap = Paint.Cap.ROUND 
+    save()
+    translate(w / 2, gap * (i + 1))
+    rotate(angleDeg * sc2)
+    for (j in 0..(lines - 1)) {
+        save()
+        drawSideOpenLine(size * (1f - 2 * j), sc1.divideScale(j, lines), paint)
+        restore()
+    }
+    restore()
+}
